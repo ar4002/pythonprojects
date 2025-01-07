2ce4a8314b84c6dcd7dc012df6bbf61646ef2b96
ar4002 <killer24devil68@gmail.com>
2025-01-07 15:15:21 +0530

    Add files via upload
    Mega project 1- JArvis

---
+++ client.py
@@ -0,0 +1,12 @@
+from openai import OpenAI
+
+
+client = OpenAI(
+  api_key="<Your Key Here>",
+)
+
+  ]
+)
+
+print(completion.choices[0].message.content)
+print(completion.choices[0].message.content)
